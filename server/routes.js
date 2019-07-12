1fd72ea826a57df9d1ae5b5e14310c1e9bd99ffe
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2019-07-12 17:17:05 +0200

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,7 @@ const user = require('./models/User')
 
 router.get('/users', user.users)
 
-router.get('/user/get/:id', user.userById)
+router.post('/user/info', user.userInfo)
 router.post('/user/login', user.userLogin)
 router.post('/user/create', user.userCreate)
 router.post('/user/update/:id', user.userUpdate)
