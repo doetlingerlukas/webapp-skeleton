687fe5d8764b3b000ff67b403916d7e60dac92f9
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2020-08-30 13:01:25 +0200

    Create package for server.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const PORT = 4000;
-const routes = require('./routes')
+const routes = require('./routes');
 
 // Connect to database
 require('./db');
 
 let User = require('./models/User')
 
-app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/', routes)
